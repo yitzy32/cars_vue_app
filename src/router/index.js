ccf1cb44d468d8024e33946e2434aba2086ebaaf
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-07 16:48:47 -0400

    cars index action

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import CarsIndex from "../views/CarsIndex.vue"
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +9,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/cars',
+    name: 'CarsIndex',
+    component: CarsIndex
   },
   {
     path: '/about',
---
+++ src/views/CarsIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p v-for="car in cars">
+      Color: {{ car.color }} <br>
+      Year: {{ car.year }}
+      </p>
+  </div>
+</template>
+<style></style>
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      cars: [],
+    };
+  },
+  created: function () {
+    console.log(this.carsIndex());
+  },
+  methods: {
+    carsIndex: function () {
+      console.log("in cars index");
+      axios.get("api/cars").then((response) => {
+        console.log(response.data);
+        this.cars = response.data;
+      });
+    },
+  },
+};
+</script>
